a0ac428202e570e812ee70452eb85c1f3236fe74
StreetStrider <teamfortresslife@gmail.com>
2014-07-30 20:18:09 +0400

    expr.

---
+++ expr.js
@@ -0,0 +1,31 @@
+
+
+
+module.exports = function expr (expr)
+{
+	expr = expr.split('%%');
+
+	expr = expr.map(replN);
+	expr = expr.map(repl);
+
+	expr = expr.join('%');
+	expr = 'return '+ expr;
+
+	return new Function(expr);
+}
+
+function repl (str)
+{
+	return str.replace('%', 'arguments[0]');
+}
+
+function replN (str)
+{
+	return str.replace(reg, function (_, d)
+	{
+		d = d - 1;
+		return 'arguments['+ d +']';
+	});
+}
+
+var reg = /%(\d+)/;
