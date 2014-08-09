3fbc235e2f59d1f869b925a0d97f4c31e05efe85
StreetStrider <teamfortresslife@gmail.com>
2014-08-09 14:55:53 +0400

    tests: array/uniq.

---
+++ test/array/uniq.js
@@ -0,0 +1,33 @@
+
+
+
+var
+	eq = require('assert').deepEqual,
+	uniq = require('../../array/uniq');
+
+describe('uniq', function ()
+{
+	var testArray = [ 3, 5, 5, 4, 5, 7, 1];
+
+	it('actually works and preserves order', function ()
+	{
+		eq([ 3, 5, 4, 7, 1 ], uniq(testArray))
+	})
+
+	it('works well with monotonous array', function ()
+	{
+		eq([ 5 ], uniq([ 5, 5, 5 ]))
+	})
+
+	it('works well with empty array', function ()
+	{
+		eq([], uniq([]))
+	})
+
+	it('works with references (objects)', function ()
+	{
+		var x = {}, y = {};
+		eq([ x, y ], uniq([ x, x, y, y ]))
+	})
+
+})
