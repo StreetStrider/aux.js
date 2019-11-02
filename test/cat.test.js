635acb71eee70d4ad3f73de5edb32042ea38108a
Strider <strange.transistor@gmail.com>
2019-11-03 01:49:18 +0300

    test cat.

---
+++ test/cat.test.js
@@ -0,0 +1,52 @@
+
+import cat from '../cat'
+
+
+describe('cat', () =>
+{
+	it('cat()', () =>
+	{
+		expect(cat()).deep.eq([])
+	})
+
+	it('cat(primitive)', () =>
+	{
+		expect(cat(1)).deep.eq([ 1 ])
+	})
+
+	it('cat(primitive, ...)', () =>
+	{
+		expect(cat(1, 2, 3)).deep.eq([ 1, 2, 3 ])
+	})
+
+	it('cat(list)', () =>
+	{
+		expect(cat([ 1 ])).deep.eq([ 1 ])
+		expect(cat([ 1, 2 ])).deep.eq([ 1, 2 ])
+	})
+
+	it('cat(list, ...)', () =>
+	{
+		expect(cat([ 1 ], [ 2 ], [ 3 ])).deep.eq([ 1, 2, 3 ])
+		expect(cat([ 1, 2 ], [ 3 ], [ 4 ])).deep.eq([ 1, 2, 3, 4 ])
+		expect(cat([ 1 ], [ 2, 3 ], [ 4, 5 ])).deep.eq([ 1, 2, 3, 4, 5 ])
+	})
+
+	it('cat(mixed)', () =>
+	{
+		expect(cat(1, [ 2 ], 3, [ 4, 5 ])).deep.eq([ 1, 2, 3, 4, 5 ])
+	})
+
+	it('skips undefined', () =>
+	{
+		expect(cat(void 0)).deep.eq([])
+		expect(cat(void 0, void 0)).deep.eq([])
+		expect(cat(void 0, [ void 0 ])).deep.eq([])
+		expect(cat([ void 0, void 0 ])).deep.eq([])
+
+		expect(cat(void 0, 1)).deep.eq([ 1 ])
+		expect(cat(void 0, 1, void 0)).deep.eq([ 1 ])
+		expect(cat(void 0, [ void 0, 1 ], 2)).deep.eq([ 1, 2 ])
+		expect(cat(1, [ void 0, 2, void 0 ], [ 3 ])).deep.eq([ 1, 2, 3 ])
+	})
+})
