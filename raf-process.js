8cd3b68102634312b6375646f3b893a276ddc2c9
Strider <strange.transistor@gmail.com>
2020-06-28 01:31:12 +0300

    more raf.

--- raf-process.js
+++
@@ -1,1 +0,0 @@
-// TODO: raf process
--- raf.js
+++ raf.js
@@ -1,2 +1,47 @@
 
+
 export default requestAnimationFrame
+
+
+export function raf (fn)
+{
+	var r = requestAnimationFrame(fn)
+
+	return function disposer ()
+	{
+		if (! r) { return }
+
+		cancelAnimationFrame(r)
+
+		r = null
+	}
+}
+
+
+export function cyclic (fn)
+{
+	var r
+
+	sked()
+
+	function sked ()
+	{
+		r = requestAnimationFrame(next)
+	}
+
+	function next (...args)
+	{
+		fn(...args)
+
+		sked()
+	}
+
+	return function disposer ()
+	{
+		if (! r) { return }
+
+		cancelAnimationFrame(r)
+
+		r = null
+	}
+}
