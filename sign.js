ae213073d9f45eb8fa956022f7cedddaa6b64c38
Strider <strange.transistor@gmail.com>
2021-06-28 01:21:37 +0300

    sign.

--- sign.js
+++ sign.js
@@ -1,2 +1,26 @@
-// TODO: sign
-// TODO: sign0
+
+
+export default function sign (value)
+{
+	if (value >= 0)
+	{
+		return 1
+	}
+	else
+	{
+		return -1
+	}
+}
+
+
+export function sign0 (value)
+{
+	if (value === 0)
+	{
+		return 0
+	}
+	else
+	{
+		return sign(value)
+	}
+}
