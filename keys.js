4cd4fdf8a31a1a297213da4875b86305136c6c3b
Strider <strange.transistor@gmail.com>
2021-06-30 01:50:21 +0300

    keys.

--- keys.js
+++ keys.js
@@ -1,1 +1,64 @@
-// TODO
+// TODO: test
+
+var defaults =
+{
+	own: false,
+	deep: false,
+}
+
+export default function keys (object, options)
+{
+	options = { ...defaults, ...options }
+
+	if (options.is_deep)
+	{
+		return keys_deep(object, options.own)
+	}
+	else
+	{
+		return keys_flat(object, options.own)
+	}
+}
+
+function keys_deep (object, is_own)
+{
+	var R = null
+
+	while (object)
+	{
+		var K = keys_flat(object, is_own)
+
+		if (! R)
+		{
+			R = K
+		}
+		else
+		{
+			for (var k of K) { add(R, k) }
+		}
+
+		object = Object.getPrototypeOf(object)
+	}
+
+	return (R || [])
+}
+
+function keys_flat (object, is_own)
+{
+	if (! is_own)
+	{
+		return Object.keys(object)
+	}
+	else
+	{
+		return Object.getOwnPropertyNames(object)
+	}
+}
+
+function add (seq, value)
+{
+	if (! seq.includes(value))
+	{
+		seq.push(value)
+	}
+}
